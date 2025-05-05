a10f35bb218e328bc96d90b60b3ebea3bbdff85f
Qelris <153022149+Qelvah@users.noreply.github.com>
2025-05-05 14:54:17 -0400

    Project javadoc

--- app/src/main/java/com/example/tp2_mobile2025/project/Project.java
+++ app/src/main/java/com/example/tp2_mobile2025/project/Project.java
@@ -1,6 +1,4 @@
-package com.example.tp2_mobile2025;
-
-import java.lang.reflect.Array;
+package com.example.tp2_mobile2025.project;
 
 public class Project {
     int id = 0;
@@ -25,35 +23,51 @@ public class Project {
     }
 
     // Setter et Getter pour l'Id du projet.
+    /** @param newId est int */
     public void setId(int newId) {this.id = newId; }
+    /** @return this.id, int */
     public int getId() {return this.id; }
 
     // Setter et Getter pour le nom du projet.
+    /** @param newName est int */
     public void setName(String newName) {this.name =newName; }
+    /** @return this.name, int */
     public String getName() {return this.name; }
 
     // Setter et Getter pour la description d'un projet.
+    /** @param newDescription est String */
     public void setDescrption(String newDescription) {this.description = newDescription; }
+    /** @return this.description, String */
     public String getDescription() {return this.description; }
 
     // Setter et Getter pour le minimum de membres dans une équipe dans le projet.
+    /** @param newMin est int plus grand que 0 */
     public void setMinPetTeam(int newMin) {this.min_per_team = newMin; }
+    /** @return this.min_per_team, int plus grand que 0 */
     public int getMinPerTeam() {return this.min_per_team; }
 
     // Setter et Getter pour le maximum de membres dans une équipe dans le projet
+    /** @param newMax est int plus petit que 99 */
     public void setMax_per_team(int newMax) {this.max_per_team = newMax; }
+    /** @return max_per_team, int plus petit que 99 */
     public int getMaxPerTeam() {return this.max_per_team; }
 
     // Setter et Getter pour le statut rejoignable des équipes du projet.
+    /** @param  newJoinable est boolean */
     public void setJoinable(boolean newJoinable) {this.joinable = newJoinable; }
+    /** @return this.joinable, boolean */
     public boolean getJoinable() {return this.joinable; }
 
     // Setter et Getter pour le statut de création d'équipe du projet.
+    /** @param newCreatable est boolean */
     public void setCreatable(boolean newCreatable) {this.creatable = newCreatable; }
+    /** @return this.creatable, boolean */
     public boolean getCreatable() {return this.creatable; }
 
     // Setter et Getter pour le statut des cours commun requis pour les équipes du projet.
+    /** @param newCommon est boolean */
     public void setCommonClasses(boolean newCommon) {this.common_classes = newCommon; }
+    /** @return this.common_classes, boolean*/
     public boolean getCommonClasses() {return this.common_classes; }
 
 }
--- app/src/main/java/com/example/tp2_mobile2025/project/ProjectController.java
+++ app/src/main/java/com/example/tp2_mobile2025/project/ProjectController.java
@@ -1,21 +1,35 @@
-package com.example.tp2_mobile2025;
+package com.example.tp2_mobile2025.project;
+
+import com.example.tp2_mobile2025.Comment;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ProjectController {
 
-    /**
-     * Display a listing of the resource.
-     */
+    /** Retourne une liste d'objet Projet pour l'affichage de la liste des projets.
+     * @return ArrayList of Project objects */
     public ArrayList<Project> index() throws JSONException {
+        // ArrayList pour retour de liste de projets
         ArrayList<Project> projects = new ArrayList<Project>();
+
+        // JSONArray qui reçois la retour API de la liste des projets en json.
         JSONArray projectArray = new JSONArray();
 
+        // API CALL TO LOCAL
+        new Thread(() -> {
+            try {
+
+            }
+            catch(Exception e) {
+
+            }
+        });
+
+        // Convertit le JSON en objet Projets et les rajoutent dans l'Arraylist.
         for (int i = 0 ; i < projectArray.length() ; i++) {
             JSONObject project = projectArray.getJSONObject(i);
             Project newProject = new Project(project.getInt("id"), project.getString(
