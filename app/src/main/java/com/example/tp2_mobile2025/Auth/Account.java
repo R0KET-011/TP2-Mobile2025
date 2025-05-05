ee153da1aa0a7048152e549253244d25ee7c0632
Mari0no1 <teamshadows07@gmail.com>
2025-05-05 16:36:01 -0400

    password getters/setters

--- app/src/main/java/com/example/tp2_mobile2025/Auth/Account.java
+++ app/src/main/java/com/example/tp2_mobile2025/Auth/Account.java
@@ -36,6 +36,14 @@ public class Account {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getToken() {
         return token;
     }
