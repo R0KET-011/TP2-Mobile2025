e7f70341ab97da1b9a8a81eea8fbd55ab33f6a64
Qelris <153022149+Qelvah@users.noreply.github.com>
2025-05-05 14:09:54 -0400

    Début classe et controlleur projet

--- app/src/main/java/com/example/tp2_mobile2025/Project.java
+++ app/src/main/java/com/example/tp2_mobile2025/Project.java
@@ -1,4 +1,59 @@
 package com.example.tp2_mobile2025;
 
+import java.lang.reflect.Array;
+
 public class Project {
+    int id = 0;
+    String name = "";
+    String description = "";
+    int min_per_team = 0;
+    int max_per_team = 0;
+    boolean joinable = false;
+    boolean creatable = false;
+    boolean common_classes = false;
+
+    public Project(int newId, String newName, String newDescription, int newMin, int newMax,
+                   boolean newJoinable, boolean newCreatable, boolean newCommon ) {
+        this.id = newId;
+        this.name = newName;
+        this.description = newDescription;
+        this.min_per_team = newMin;
+        this.max_per_team = newMax;
+        this.joinable = newJoinable;
+        this.creatable = newCreatable;
+        this.common_classes = newCommon;
+    }
+
+    // Setter et Getter pour l'Id du projet.
+    public void setId(int newId) {this.id = newId; }
+    public int getId() {return this.id; }
+
+    // Setter et Getter pour le nom du projet.
+    public void setName(String newName) {this.name =newName; }
+    public String getName() {return this.name; }
+
+    // Setter et Getter pour la description d'un projet.
+    public void setDescrption(String newDescription) {this.description = newDescription; }
+    public String getDescription() {return this.description; }
+
+    // Setter et Getter pour le minimum de membres dans une équipe dans le projet.
+    public void setMinPetTeam(int newMin) {this.min_per_team = newMin; }
+    public int getMinPerTeam() {return this.min_per_team; }
+
+    // Setter et Getter pour le maximum de membres dans une équipe dans le projet
+    public void setMax_per_team(int newMax) {this.max_per_team = newMax; }
+    public int getMaxPerTeam() {return this.max_per_team; }
+
+    // Setter et Getter pour le statut rejoignable des équipes du projet.
+    public void setJoinable(boolean newJoinable) {this.joinable = newJoinable; }
+    public boolean getJoinable() {return this.joinable; }
+
+    // Setter et Getter pour le statut de création d'équipe du projet.
+    public void setCreatable(boolean newCreatable) {this.creatable = newCreatable; }
+    public boolean getCreatable() {return this.creatable; }
+
+    // Setter et Getter pour le statut des cours commun requis pour les équipes du projet.
+    public void setCommonClasses(boolean newCommon) {this.common_classes = newCommon; }
+    public boolean getCommonClasses() {return this.common_classes; }
+
 }
--- app/src/main/java/com/example/tp2_mobile2025/ProjectController.java
+++ app/src/main/java/com/example/tp2_mobile2025/ProjectController.java
@@ -1,12 +1,30 @@
 package com.example.tp2_mobile2025;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ProjectController {
+
     /**
      * Display a listing of the resource.
      */
-    public void index()
-    {
-        //
+    public ArrayList<Project> index() throws JSONException {
+        ArrayList<Project> projects = new ArrayList<Project>();
+        JSONArray projectArray = new JSONArray();
+
+        for (int i = 0 ; i < projectArray.length() ; i++) {
+            JSONObject project = projectArray.getJSONObject(i);
+            Project newProject = new Project(project.getInt("id"), project.getString(
+                    "name"), project.getString("description"), project.getInt("min_per_team"), project.getInt("max_per_team"), project.getBoolean("joinable"), project.getBoolean("creatable"), project.getBoolean("common_classes"));
+
+            projects.add(newProject);
+        }
+
+        return projects;
     }
 
     /**
