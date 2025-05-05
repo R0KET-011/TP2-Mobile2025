4c6f18c162b5509b77944cacbc7286ed2e525638
Animo105 <emeric2005leclerc@gmail.com>
2025-05-05 16:54:00 -0400

    créations des helper, début de bd et d'élève

---
+++ app/src/main/java/com/example/tp2_mobile2025/students/StudentsListActivity.java
@@ -0,0 +1,26 @@
+package com.example.tp2_mobile2025.students;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tp2_mobile2025.R;
+
+public class StudentsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_students_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
