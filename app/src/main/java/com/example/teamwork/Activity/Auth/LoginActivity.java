5d053419c41e8aea741911e91883420f447ef22c
IAmSympathy <teamshadows07@gmail.com>
2025-05-07 10:12:14 -0400

    Remake des views avec nouveau visuel

--- app/src/main/java/com/example/teamwork/Activity/Auth/LoginActivity.java
+++ app/src/main/java/com/example/teamwork/Activity/Auth/LoginActivity.java
@@ -17,13 +17,8 @@ package com.example.teamwork.Activity.Auth;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.teamwork.R;
 
@@ -32,17 +27,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-        Button buttonRegister = findViewById(R.id.buttonRegister);
-        buttonRegister.setOnClickListener(this);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 
     @Override
