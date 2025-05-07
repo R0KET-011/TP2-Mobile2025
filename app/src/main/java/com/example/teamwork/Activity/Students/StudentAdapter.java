a4a6a1f799ed89540b41c568d50ecb86a0ac1868
Animo105 <emeric2005leclerc@gmail.com>
2025-05-07 11:13:31 -0400

    remake des students

---
+++ app/src/main/java/com/example/teamwork/Activity/Students/StudentAdapter.java
@@ -0,0 +1,91 @@
+package com.example.teamwork.Activity.Students;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.teamwork.Database.Tables.Student;
+import com.example.teamwork.R;
+
+/**
+ * L'adapteur pour le RecyclerView de la liste des élèves.
+ */
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.ViewHolder> {
+
+    /**
+     * La liste des élèves à afficher.
+     */
+    private Student[] students;
+    /**
+     * Le Context ou le recyclerview est.
+     */
+    private Context context;
+
+    /**
+     * Constructeur de l'adapter qui requière la liste des élèves et le context
+     * @param context
+     * @param students
+     */
+    public StudentAdapter(Context context, Student[] students) {
+        this.context = context;
+        this.students = students;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.rv_students_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tv_name.setText(students[position].getFullName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return students.length;
+    }
+
+    /**
+     * ViewHolder pour le layout du RecyclerView
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        /**
+         * TextView qui contien le nom de l'élève
+         */
+        TextView tv_name;
+        /**
+         * ImageView qui contien l'icone de commentaire
+         */
+        ImageView iv_comment;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_name = itemView.findViewById(R.id.tv_nom);
+            iv_comment = itemView.findViewById(R.id.comment_icon);
+
+            iv_comment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/teamwork/Activity/Students/StudentListActivity.java
@@ -0,0 +1,30 @@
+package com.example.teamwork.Activity.Students;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.teamwork.R;
+
+public class StudentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_student_list);
+
+        RecyclerView RVstudent = findViewById(R.id.rv_students);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
