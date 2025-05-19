1d0fdf36e38b81c13bd69e915ec8aae81736d26a
Qelris <153022149+Qelvah@users.noreply.github.com>
2025-05-18 23:33:08 -0400

    ApiClient retrofitReset => Pour rafraichir la connexion avec un token valide.

--- app/src/main/java/com/example/teamwork/API/ApiClient.java
+++ app/src/main/java/com/example/teamwork/API/ApiClient.java
@@ -18,9 +18,12 @@
 
 package com.example.teamwork.API;
 
+import android.util.Log;
+
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ApiClient {
     private static Retrofit retrofit = null;
@@ -29,8 +32,13 @@ public class ApiClient {
      * @param token est token authentification */
     public static Retrofit getClient(String token) {
         if (retrofit == null) {
+
+            HttpLoggingInterceptor logs = new HttpLoggingInterceptor();
+            logs.setLevel(HttpLoggingInterceptor.Level.BODY);
+
             OkHttpClient client =
-                    new OkHttpClient.Builder().addInterceptor(new AuthInterceptor(token)).build();
+                    new OkHttpClient.Builder().addInterceptor(new AuthInterceptor(token))
+                            .addInterceptor(logs).build();
 
             retrofit = new Retrofit.Builder()
                     .baseUrl("http://10.0.2.2:8000/api/")
@@ -40,6 +48,11 @@ public class ApiClient {
         }
 
         return retrofit;
+    }
+
+    public static void retrofitReset() {
+        Log.v("API", "Retrofit Reset.");
+        retrofit = null;
     }
 
 }
